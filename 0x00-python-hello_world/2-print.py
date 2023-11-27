e0f98ef717088935c13a88f2a22455caef74c8e8
Shayne999 <shaynejuniorn@gmail.com>
2023-11-27 10:20:33 -0800

    Halfway

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,2 @@
+#!bin/bash/pyhton3
+print('"Programming is like building a multilingual puzzle')
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/bin/bash/python3
+str = "Holberton School"
+print(str + str + str)
+print(str[:9])
