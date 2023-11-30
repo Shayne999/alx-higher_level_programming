a9d753b1ed2c8edab09c369739ec2b7fcf283c12
Shayne999 <shaynejuniorn@gmail.com>
2023-11-30 05:13:03 -0800

    Q1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-if __name__ = " __main__":
+if __name__ = " __main__"i:
     form add_0 import add
 
     a = 1
     b = 2
 
-    add(a + b)
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
