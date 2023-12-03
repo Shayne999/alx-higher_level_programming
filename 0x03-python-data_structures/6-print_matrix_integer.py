99c696058096e147c32097f43e21748e97269404
Shayne999 <shaynejuniorn@gmail.com>
2023-12-03 02:48:05 -0800

    Q6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(10):
+        for j in range(len(matrix[i])):
+            if j == 9:
+                print(matrix[i][j], end="")
+            else:
+                print("{}".format(matrix[i][j]), end=", ")
