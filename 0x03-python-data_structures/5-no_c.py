21adaa5c414104780089df300a028c24a4a55a73
Shayne999 <shaynejuniorn@gmail.com>
2023-12-03 02:34:42 -0800

    Q5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-        mod_string = my_string.replace("c", "")
-        print(mod_string)
+        mod_string = my_string.replace("c", "").replace("C", "")
+        return(mod_string)
