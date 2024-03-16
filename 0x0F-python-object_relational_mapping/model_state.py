f9933fb10a71ea5a44e02ceff2899913b461c369
Shayne999 <shaynejuniorn@gmail.com>
2024-03-16 13:20:07 +0000

    Complete

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
+""" Contains State class """
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
 
 class State(Base):
+    """
+    Class with id and name attributes of each state
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
-if __name__ == "__main__":
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-
-    DB_URL = 'mysql://username:password@localhost:3306/database_name'
-    engine = create_engine(DB_URL)
-
-    from model_state import State
-
-    Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
